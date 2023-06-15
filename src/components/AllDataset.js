b929a12a8be663e6575a2bf3139b7391c397e06e
isha-mistry <ishamistry.lamprostech@gmail.com>
2023-06-15 09:56:04 +0530

    pulling updated code

--- src/components/AllDataset.js
+++ src/components/AllDataset.js
@@ -1,18 +1,49 @@
 import React from "react";
 import { data } from "../dummyData/dataset";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { modelInstance } from "./Contract";
+import lighthouse from "@lighthouse-web3/sdk";
 
 function AllDataset(props) {
+  const [allDataset, setAllDataset] = useState([]);
+
+  const getAllDatasets = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        if (!provider) {
+          console.log("Metamask is not installed, please install!");
+        }
+        const con = await modelInstance();
+        const getDataset = await con.getAllDataSet();
+        setAllDataset(getDataset);
+        console.log(allDataset);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    async function fetchDatasets() {
+      await getAllDatasets();
+    }
+    fetchDatasets();
+  }, []);
+
   return (
     <div className="main-dataset-grid-profile">
-      {data.map((item, index) => (
+      {allDataset.map((item, index) => (
         <>
           <div style={{ width: "100%" }}>
-            <img
+            {/* <img
               src={item.image_url}
               alt={`Image ${index}`}
-              className="dataset-img"
-            />
+              className="dataset-image"
+            /> */}
             <div>
               <h4 key={index}>{item.name}</h4>
               <div key={index}>
