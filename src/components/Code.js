44bacb6c168da4aa07c11ce455fd5eb282336432
Lajja123 <lajja.lamprostech@gmail.com>
2023-06-18 15:59:18 +0530

    add code IDE

--- src/components/Code.js
+++ src/components/Code.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
+import MonacoEditor from "./MonacoEditor";
 
 function Code() {
-  return <div style={{ margin: 20 }}>Code</div>;
-}
+  const [code, setCode] = useState('');
+  const handleRunCode = () => {
+    try {
+      // Use eval or any other method to execute the code
+      eval(code);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <MonacoEditor code={code} onRunCode={handleRunCode} />
+      <button onClick={handleRunCode}>Run Code</button>
+    </div>
+  );
+};
+
 
 export default Code;
