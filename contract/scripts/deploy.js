dc8fad2b92da2fa77333742a4d074283207587ed
Lajja123 <lajja.lamprostech@gmail.com>
2023-06-19 12:18:23 +0530

    final changes

--- contract/scripts/deploy.js
+++ contract/scripts/deploy.js
@@ -1,13 +1,11 @@
-const { ethers } = require("hardhat");
-
+import { ethers } from "hardhat";
 
 async function deploy() {
-    const ContractName = await ethers.getContractFactory("ModelValidationPlatform");
-    const contract = await ContractName.deploy();
-    console.log(" Contract deployed to address:", contract.address);
-  }
-  
- deploy();
+  const ContractName = await ethers.getContractFactory(
+    "ModelValidationPlatform"
+  );
+  const contract = await ContractName.deploy();
+  console.log(" Contract deployed to address:", contract.address);
+}
 
-
- 
+deploy();
